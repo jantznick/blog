99ffcd8302b3c1c4496db974137fbfffe7d4ba92
Nick Jantz <thenickjantz@gmail.com>
2024-02-01 10:53:47 -0600

    first try at service worker

--- .eleventy.js
+++ .eleventy.js
@@ -9,9 +9,10 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
-  // Copy the `img` and `css` folders to the output
+  // Copy the `img`, `js` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("CNAME");
 
   // Add plugins
